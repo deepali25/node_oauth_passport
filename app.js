5b1fc70649d3a99535d4ca4f61fc840734655b93
deepak <deepamdeebu@gmail.com>
2023-12-11 19:58:27 +0530

    Mongoose : Store profile information in the mongodb and pass the mongo db id for further processing

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const keys = require('./config/keys');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI)
+  .then(result => app.listen(4000))
+  .catch(err => console.log(err));
 
 // set up routes
 app.use('/auth', authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = require('../models/user-model');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -10,7 +21,21 @@ passport.use(
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
-        console.log("Came to callback function");
-        console.log(profile);
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        });
     })
 );
