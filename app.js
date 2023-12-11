f27001530a31de11ae1119f0a6f64ac5746696a3
deepak <deepamdeebu@gmail.com>
2023-12-11 20:33:57 +0530

    Cookie : Store the mongo db id to cookie and by serializing and de-serializing the id to checked for login users

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -13,6 +15,16 @@ app.set('view engine', 'ejs');
 mongoose.connect(keys.mongodb.dbURI)
   .then(result => app.listen(4000))
   .catch(err => console.log(err));
+
+// set up session cookies
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // set up routes
 app.use('/auth', authRoutes);
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -19,6 +19,7 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.send(req.user);
     res.send('you reached the redirect URI');
 });
 
