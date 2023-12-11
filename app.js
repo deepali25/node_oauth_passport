9021a8db2db2c52a1246d1bdc08da6d736c28184
deepak <deepamdeebu@gmail.com>
2023-12-11 20:59:00 +0530

    Profile : Redirect to the profile page once after login and dont go the the login page on further page reload till logout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
@@ -28,6 +29,7 @@ app.use(passport.session());
 
 // set up routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 // create home route
 app.get('/', (req, res) => {
